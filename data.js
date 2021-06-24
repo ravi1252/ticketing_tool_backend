69f04d17ca51a1bde6dfc8b61c230d38bb0e7f95
ravi1252 <vadlakonda.ravikiran@gmail.com>
2021-06-24 22:23:46 +0800

    data file

---
+++ data.js
@@ -0,0 +1,85 @@
+module.exports = {
+  tickets : [
+    {
+        "id" : 1,
+        "name" : "Customer grievance",
+        "description" : "Network not available",
+        "createdDate" : "23 Sep 2020",
+        "status" : "unresolved",
+        "assignedToEmail" : "technician#myrepublic.com"
+    },
+      {
+          "id" : 2,
+          "name" : "Customer grievance",
+          "description" : "Network not available",
+          "createdDate" : "23 Sep 2021",
+          "status" : "resolved",
+          "assignedToEmail" : "technician#myrepublic.com"
+      },
+        {
+            "id" : 3,
+            "name" : "Customer grievance",
+            "description" : "Network not available",
+            "createdDate" : "23 Sep 2020",
+            "status" : "unresolved",
+            "assignedToEmail" : "technician#myrepublic.com"
+        },
+          {
+              "id" : 4,
+              "name" : "Customer grievance",
+              "description" : "Network not available",
+              "createdDate" : "23 Sep 2021",
+              "status" : "resolved",
+              "assignedToEmail" : "technician#myrepublic.com"
+          },
+            {
+                "id" : 5,
+                "name" : "Customer grievance",
+                "description" : "Network not available",
+                "createdDate" : "23 Sep 2020",
+                "status" : "unresolved",
+                "assignedToEmail" : "technician#myrepublic.com"
+            },
+              {
+                  "id" : 6,
+                  "name" : "Customer grievance",
+                  "description" : "Network not available",
+                  "createdDate" : "23 Sep 2021",
+                  "status" : "resolved",
+                  "assignedToEmail" : "technician#myrepublic.com"
+              },
+                {
+                    "id" : 7,
+                    "name" : "Customer grievance",
+                    "description" : "Network not available",
+                    "createdDate" : "23 Sep 2020",
+                    "status" : "unresolved",
+                    "assignedToEmail" : "technician#myrepublic.com"
+                },
+                  {
+                      "id" : 8,
+                      "name" : "Customer grievance",
+                      "description" : "Network not available",
+                      "createdDate" : "23 Sep 2021",
+                      "status" : "resolved",
+                      "assignedToEmail" : "technician#myrepublic.com"
+                  },
+                    {
+                        "id" : 9,
+                        "name" : "Customer grievance",
+                        "description" : "Network not available",
+                        "createdDate" : "23 Sep 2020",
+                        "status" : "unresolved",
+                        "assignedToEmail" : "technician#myrepublic.com"
+                    },
+                      {
+                          "id" : 10,
+                          "name" : "Customer grievance",
+                          "description" : "Network not available",
+                          "createdDate" : "23 Sep 2020",
+                          "status" : "unresolved",
+                          "assignedToEmail" : "technician#myrepublic.com"
+                      }
+  ]
+
+}

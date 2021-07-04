84a4b7a5eb97c4286bf973d1388c8d321baed50e
ravi1252 <vadlakonda.ravikiran@gmail.com>
2021-07-05 00:07:49 +0800

    changees

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var tools = require('./data');
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 4000
 
@@ -45,6 +46,37 @@ app.get('/tickets', (req, res) => {
   res.send(tools.tickets)
 })
 
+app.post('/getUserDetails', (req, res) => {
+  console.log("req.params.mobilenumber : " + req.params.mobilenumber);
+  console.log("req.params.emailid : " + req.params.emailid);
+  return res.send({
+      "id" : 1,
+      "name" : "Ramanathan",
+      "fatherName" : "Network not available",
+      "emailId" : "23 Sep 2021",
+      "dob" : "resolved",
+      "activationTime" : "technician#myrepublic.com",
+      "address" : {
+        "city" : "kukatpally",
+        "area" : "hyderabad",
+        "houseNo" : "310/A"
+      }
+  });
+})
+
+app.get('/ticketoptions', (req, res) => {
+  console.log("entered ticketoptions");
+    return res.send(
+      [
+        {"key" : "1", "value" : "Customer facing internet issue"},
+        {"key" : "2", "value" : "Pack issue"},
+        {"key" : "3", "value" : "Router issue"},
+        {"key" : "4", "value" : "Low internet"},
+        {"key" : "5", "value" : "Cost issue"},
+        {"key" : "6", "value" : "Others"}
+      ]
+    );
+})
 
 app.delete('/tickets/:id', (req, res) => {
   console.log("req.params.id : " + req.params.id);
